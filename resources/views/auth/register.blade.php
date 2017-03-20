35680575689898e396fd6541c07f8fee1ac4572b
ilyas <ilyas.ilyasov.1@gmail.com>
2017-03-21 03:06:08 +0400

    register login views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section("title", " | Register")
 
-@section
+@section("content")
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
         {!! Form::open() !!}
@@ -14,7 +14,7 @@
             {{ Form::password('password',['class'=>'form-control']) }}
 			{{ Form::label('password_confirmation','Confirm password:') }}
             {{ Form::password('password_confirmation',['class'=>'form-control']) }}
-            {{ Form::submit('Register',['class'=>'btn btn-primary btn-block', 'style'=>'margin-top:30px;']) }}
+            {{ Form::submit('Register',['class'=>'btn btn-primary btn-block','style'=>'margin-top:20px;']) }}
         {!! Form::close() !!}
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@
 
 
 Route::group(["middleware"=>["web"]], function(){
+
+    Route::get('auth/login','Auth\LoginController@showLoginForm');
+    Route::post('auth/login','Auth\AuthController@postLogin');
+    Route::post('auth/logout','Auth\AuthController@postLogout');
+
+    Route::get('auth/register','Auth\RegisterController@showRegistrationForm');
+    Route::post('auth/register','Auth\AuthController@postRegister');
+
     Route::get('/', "SehifeController@getIndex");
 
     Route::get('/about', "SehifeController@getAbout");
