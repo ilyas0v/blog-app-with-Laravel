9831658c7f13f82c4b58d123744e2d739df013db
ilyas0v <ilyas.ilyasov.1@gmail.com>
2017-04-16 17:08:26 +0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceScheme('https');
+        }
     }
 
     /**
